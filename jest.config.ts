d84882d859dc5a23e937af9dc9df64f2c0633ecc
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2024-10-23 20:27:59 -0300

    SUcesso gerando relatorio html

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,8 @@
 import type {Config} from 'jest';
 
 const config: Config = {
-  collectCoverageFrom: [
-    '**/*.{js,jsx}',
-    '!**/node_modules/**',
-    '!**/vendor/**',
-  ],
   preset: "jest-preset-angular",
   setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
-  globalSetup: 'jest-preset-angular/global-setup',
   testPathIgnorePatterns: [
     "<rootDir>/node_modules/",
     "<rootDir>/dist/"
@@ -19,6 +13,32 @@ const config: Config = {
       stringifyContentPathRegex: "\\.html$"
     }]
   },
+  collectCoverageFrom: [
+    'src/app/**/*.ts', // Restringe a coleta de cobertura apenas para a pasta app
+    '!**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+    '!src/app/**/*.module.ts', // Exclui arquivos de módulo
+    '!src/app/**/*.array.ts', // Exclui arquivos de array
+    '!src/app/**/*.model.ts', // Exclui arquivos de modelo
+    '!src/app/**/*.enum.ts', // Exclui arquivos de enum
+    '!src/app/**/*.interface.ts' // Exclui arquivos de interface
+  ],
+  globalSetup: 'jest-preset-angular/global-setup',
+  moduleFileExtensions: ['ts', 'html', 'js', 'json'],
+  coverageReporters: ['html', 'lcov', 'text-summary'],
+  collectCoverage: true,
+  coverageDirectory: '<rootDir>/coverage/',
+  reporters: [
+    'default',
+    ['jest-junit', { outputDirectory: './reports/junit', outputName: 'junit.xml' }],
+    ['jest-html-reporter', {
+      pageTitle: 'Test Report',
+      outputPath: './reports/test-report.html',
+      includeFailureMsg: true,
+      includeConsoleLog: true
+    }]
+  ],
 };
 
 export default config;    
