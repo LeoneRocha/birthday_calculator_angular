311c2cf76c356d58117806df63462f42667fd790
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2024-11-02 20:18:31 -0300

    Adiconando rodape

--- src/app/components/birthday-form/birthday-form.component.ts
+++ src/app/components/birthday-form/birthday-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { BirthdayService } from '../../services/birthday.service';
+
 @Component({
   selector: 'app-birthday-form',
   templateUrl: './birthday-form.component.html',
@@ -11,18 +12,20 @@ export class BirthdayFormComponent implements OnInit {
   birthdayForm: FormGroup;
   result: any;
   countdown$: Observable<string> = new Observable<string>();
+  currentYear: number;
+  appVersion: string;
 
   constructor(private readonly fb: FormBuilder, private readonly birthdayService: BirthdayService) {
     this.birthdayForm = this.fb.group({
       name: [''],
       birthdate: ['']
     });
+    this.currentYear = new Date().getFullYear();
+    this.appVersion = '2024.11.02'; // Certifique-se de que a versão correta seja atribuída aqui
   }
 
   ngOnInit(): void {
-
     console.log('called OnInit');
-
   }
 
   onSubmit(): void {
