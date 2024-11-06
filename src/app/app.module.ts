429dcbd92238a56fd3b83909882c0fc5463185e8
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2024-11-05 22:55:00 -0300

    Ajuste sonar cloud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'; 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { BirthdayFormComponent } from './components/birthday-form/birthday-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServerInfoComponent } from './components/server-info/server-info.component';
 import { NgIconsModule } from '@ng-icons/core';
 import { simpleAngular, simpleTypescript, simpleJest, simpleNodedotjs } from '@ng-icons/simple-icons';
-import { HoroscopeComponent } from './components/horoscope/horoscope.component'; 
-import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { HoroscopeComponent } from './components/horoscope/horoscope.component';
+import { provideHttpClient } from '@angular/common/http';
 import { BirthdateInfoService } from './services/birthdateinfoservice.service';
 import { ZodiacService } from './services/zodiac.service';
 import { NameService } from './services/name.service';
@@ -27,10 +27,10 @@ import { BirthDateService } from './services/birthdate.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule, 
-    BrowserAnimationsModule,    
-    NgIconsModule.withIcons({ simpleAngular, simpleTypescript, simpleJest, simpleNodedotjs }), 
-        
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    NgIconsModule.withIcons({ simpleAngular, simpleTypescript, simpleJest, simpleNodedotjs }),
+
   ],
   providers: [
     BirthdateInfoService,
