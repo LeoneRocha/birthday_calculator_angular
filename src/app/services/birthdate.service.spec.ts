916e2755d83f2639dab02157191b0d895260ced2
Leone Costa Rocha <leocr_lem@yahoo.com.br>
2025-12-31 00:45:17 -0300

    Ajustes dos testes

--- src/app/services/birthdate.service.spec.ts
+++ src/app/services/birthdate.service.spec.ts
@@ -67,15 +67,27 @@ describe('BirthDateService', () => {
   });
   it('should calculate next birthday correctly', () => {
     const birthdate = new Date('1990-01-01');
+    const today = new Date();
+    let expectedYear = today.getUTCFullYear();
+    const nextBirthdayDate = new Date(Date.UTC(expectedYear, 0, 2));
+    if (today > nextBirthdayDate) {
+      expectedYear++;
+    }
     const nextBirthday = service.calculateNextBirthday(birthdate);
-    expect(nextBirthday.getUTCFullYear()).toBe(2025);
+    expect(nextBirthday.getUTCFullYear()).toBe(expectedYear);
     expect(nextBirthday.getUTCMonth()).toBe(0);
     expect(nextBirthday.getUTCDate()).toBe(2);
   });
   it('should calculate age correctly', () => {
     const birthdate = new Date('1990-01-01');
+    const today = new Date();
+    let expectedAge = today.getUTCFullYear() - 1990;
+    const hasHadBirthday = (today.getUTCMonth() > 0) || (today.getUTCMonth() === 0 && today.getUTCDate() >= 2);
+    if (!hasHadBirthday) {
+      expectedAge--;
+    }
     const age = service.calculateAge(birthdate);
-    expect(age).toBe(34);
+    expect(age).toBe(expectedAge);
   });
   it("should return a Date instance when the input is already a Date", () => {
     const dateInput = new Date(1990, 3, 25);
